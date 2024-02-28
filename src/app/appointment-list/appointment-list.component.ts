ad46362eaf74bdbeda49049d9de4ea655f00c148
Giovanni Navarete <giod26@gmail.com>
2024-02-28 11:36:51 -0600

    Initial project commit on development branch

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent implements OnInit {
+
+  newAppointmentTitle:string = ""
+  newAppointmentDate:Date = new Date()
+
+  appointments: Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments')
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
+
+  addAppointment(){
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+
+      this.newAppointmentTitle = ""
+      this.newAppointmentDate = new Date();
+
+      this.updateLocalStorage()
+    }
+  }
+
+  deleteAppointment(index: number){
+    this.appointments.splice(index, 1)
+    this.updateLocalStorage()
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('appointments', JSON.stringify(this.appointments))
+  }
+
+}
